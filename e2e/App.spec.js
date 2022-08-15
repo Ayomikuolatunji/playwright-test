c7e96d57614239fbd1fb24c8f8ee34f6024bbf1a
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-08-15 17:50:44 +0100

    first commit

---
+++ e2e/App.spec.js
@@ -0,0 +1,29 @@
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async ({page}) => {
+     await page.goto('http://localhost:3001/');
+     console.log('Before tests');
+})
+
+test.describe("css selector for playwright test",()=>{
+    // check for heading 1
+    test("check for head 1",async({page})=>{
+        const name = await page.innerText('.heading1');
+       expect(name).toBe('head1');
+    })
+    // text selector
+    test("check for text",async({page})=>{
+        const name = await page.innerText('button');
+       expect(name).toBe('clicked');
+    })
+    // check for div class c
+    test("check for div class c",async({page})=>{
+        const name = await page.innerText('.c >> h1');
+        expect(name).toBe('hello');
+    })
+})
+
+test.afterAll(async ({page}) => {
+    console.log('After tests',page);
+});
+  
--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* css selector for playwright test */}
+        <h1 className='heading1'>head1</h1>
+        {/* text selector */}
+        <button>clicked</button>
+        <div className='a'>
+            <div className='b'>
+                 <div className='c'>
+                     <h1>hello</h1>
+                 </div>
+            </div>
+        </div>
     </div>
   );
 }
